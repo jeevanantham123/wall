5f1a6ea7fea27a2e7fccb50392f31e90becc6904
jeevanantham123 <jeewa.mad.2000@gmail.com>
2021-05-29 18:02:56 +0530

    home screen fix

--- components/PostCard/index.js
+++ components/PostCard/index.js
@@ -7,28 +7,29 @@ function PostCard(props) {
   return (
     <div>
       <div className="mt-20 min-h-screen overflow-y-scroll">
-        {posts?.length &&
-          posts.map((post) => {
-            return (
-              <div className="w-300 md:w-500 border shadow my-20 flex flex-col p-20">
-                <div>{post.title}</div>
-                <div className="flex mt-20 min-w-full w-full justify-center">
-                  <ReactTinyLink
-                    cardSize="small"
-                    showGraphic={true}
-                    maxLine={2}
-                    minLine={1}
-                    url={post?.link}
-                    // proxyUrl={"https://cors-anywhere.herokuapp.com/corsdemo"}
-                    onError={() => {
-                      // toast.error("Unable to Preview!");
-                      //   setPreview(false);
-                    }}
-                  />
+        {posts?.length
+          ? posts.map((post) => {
+              return (
+                <div className="w-300 md:w-500 border shadow my-20 flex flex-col p-20">
+                  <div>{post.title}</div>
+                  <div className="flex mt-20 min-w-full w-full justify-center">
+                    <ReactTinyLink
+                      cardSize="small"
+                      showGraphic={true}
+                      maxLine={2}
+                      minLine={1}
+                      url={post?.link}
+                      // proxyUrl={"https://cors-anywhere.herokuapp.com/corsdemo"}
+                      onError={() => {
+                        // toast.error("Unable to Preview!");
+                        //   setPreview(false);
+                      }}
+                    />
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })
+          : null}
       </div>
     </div>
   );
